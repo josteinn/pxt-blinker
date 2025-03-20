a8f79a31fa732958ce3992f3c9ead156e9cdb058
josteinn <jostein.nordengen@uia.no>
2025-03-20 11:01:38 +0100

    filled in the shims.d.ts

---
+++ shims.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+declare namespace blinker {
+
+    /**
+     * Adds two numbers and returns the result
+     */
+    //% shim=blinker::startBlink
+    function startBlink(timeOn: int32, timeOff: int32): int32;
+}
+
+// Auto-generated. Do not edit. Really.
